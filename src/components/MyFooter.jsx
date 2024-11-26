bc7520b996e83c38000e2f714c893fa6cfb77668
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2024-11-26 17:22:12 +0200

    Creates navbar, footer and alert

---
+++ src/components/MyFooter.jsx
@@ -0,0 +1,20 @@
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import { Row } from 'react-bootstrap'
+
+const MyFooter = () => {
+  return (
+    <div collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Row className="d-flex">
+          <Navbar.Brand href="#home">EpiBooks</Navbar.Brand>
+          <Nav.Link href="#features">Contacts</Nav.Link>
+          <Nav.Link href="#pricing">About Us</Nav.Link>
+        </Row>
+      </Container>
+    </div>
+  )
+}
+
+export default MyFooter
